d9854c71e608958fa5735e70b599942781900e1c
aifuxi <aifuxi.js@gmail.com>
2023-08-02 23:01:03 +0800

    feat✨: 获取标签列表、新增标签

--- routers/router.go
+++ routers/router.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/aifuxi/jianyu-blog-api/pkg/setting"
+	v1 "github.com/aifuxi/jianyu-blog-api/routers/api/v1"
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,6 +23,18 @@ func InitRouter() *gin.Engine {
 			"msg": "new router struct",
 		})
 	})
+
+	ap1v1 := r.Group("/api/v1")
+	{
+		// 获取标签列表
+		ap1v1.GET("/tags", v1.GetTags)
+		// 新建标签列表
+		ap1v1.POST("/tags", v1.AddTag)
+		// 更新标签列表
+		ap1v1.PUT("/tags", v1.EditTag)
+		// 删除标签列表
+		ap1v1.DELETE("/tags", v1.DeleteTag)
+	}
 
 	return r
 }
