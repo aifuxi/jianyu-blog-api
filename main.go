8401f6ef45856c397ed44531f3e430d019ec712a
aifuxi <aifuxi.js@gmail.com>
2023-08-06 01:15:15 +0800

    feat✨: 配置抽离，同一收敛到setting

--- main.go
+++ main.go
@@ -2,22 +2,29 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
+	"github.com/aifuxi/jianyu-blog-api/models"
+	"github.com/aifuxi/jianyu-blog-api/pkg/logging"
 	"github.com/aifuxi/jianyu-blog-api/pkg/setting"
 	"github.com/aifuxi/jianyu-blog-api/routers"
 )
 
 func main() {
+	setting.Setup()
+	logging.Setup()
+	models.Setup()
+
 	router := routers.InitRouter()
 
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:        router,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
-	}
+	// s := &http.Server{
+	// 	Addr:           fmt.Sprintf(":%d", setting.ServerSetting.HttpPort),
+	// 	Handler:        router,
+	// 	ReadTimeout:    setting.ServerSetting.ReadTimeout,
+	// 	WriteTimeout:   setting.ServerSetting.WriteTimeout,
+	// 	MaxHeaderBytes: 1 << 20,
+	// }
 
-	s.ListenAndServe()
+	// s.ListenAndServe()
+
+	router.Run(fmt.Sprintf(":%d", setting.ServerSetting.HttpPort))
 }
--- models/auth.go
+++ models/auth.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 type Auth struct {
 	ID       int    `gorm:"primaryKey;" json:"id"`
 	Username string `json:"username"`
@@ -8,6 +10,8 @@ type Auth struct {
 
 func CheckAuth(username, password string) bool {
 	var auth Auth
+
+	fmt.Printf("数据库引用对象db: %v\n", db)
 
 	db.Select("id").Where(Auth{Username: username, Password: password}).First(&auth)
 
